49474b1a30aafee5542cc728f9ae0f6b229dd444
HarryHuy12 <harrytheostrich@gmail.com>
2025-02-06 19:10:36 +0700

    update index

--- js/chitiet.js
+++ js/chitiet.js
@@ -50,7 +50,7 @@ async function getKitchenItem(url) {
           </div>
 
           <div class = "add">
-            <h3><button><a href="./camon.html">Add to cart</a></button></h3>
+            <h3><button><a href="./payment.html">Add to cart</a></button></h3>
           </div>
   
         </div>
